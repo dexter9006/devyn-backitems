4da4308743be87886483a593d2ae0cd5d33af0a1
dexter9006 <52006237+dexter9006@users.noreply.github.com>
2022-09-18 17:50:43 +0100

    Update BackItems.lua

--- BackItems.lua
+++ BackItems.lua
@@ -4,21 +4,21 @@ BackItems = {
         model="prop_money_bag_01", -- Model you want to display
         back_bone = 24818, -- PED bone the entity is attached to.
         -- Location of the model on the players back. Note this is and offset relative to the players bone coords.
-        x = -0.4,
-        y = -0.17,
-        z = -0.12,
+        x = -0.6,
+        y = -0.15,
+        z = -0.28,
         -- Rotaion of the object.
         x_rotation = 0.0,
-        y_rotation = 90.0,
+        y_rotation = 70.0,
         z_rotation = 0.0,
     },
 
     ["meth"] = {
         model="hei_prop_pill_bag_01", 
         back_bone = 24818,
-        x = -0.1,
-        y = -0.17,
-        z = 0.12,
+        x = -0.3,
+        y = -0.15,
+        z = 0.05,
         x_rotation = 0.0,
         y_rotation = 90.0,
         z_rotation = 0.0,
@@ -82,6 +82,17 @@ BackItems = {
         z = 0.0,
         x_rotation = 0.0,
         y_rotation = 90.0,
+        z_rotation = 0.0,
+    },
+    -- Vitto
+    ["skateboard"] = {
+        model="p_defilied_ragdoll_01_s", 
+        back_bone = 24818,
+        x = 0.05,
+        y = -0.25,
+        z = 0.05,
+        x_rotation = 268.0,
+        y_rotation = 180.0,
         z_rotation = 0.0,
     },
 }
