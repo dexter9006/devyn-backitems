7d23465fa24895eb58f974a4855878da2bd96770
dexter9006 <52006237+dexter9006@users.noreply.github.com>
2023-07-08 21:45:40 +0100

    Update BackItems.lua

--- BackItems.lua
+++ BackItems.lua
@@ -116,7 +116,7 @@ BackItems = {
         y = -0.17,
         z = -0.1,
         x_rotation = 0.0,
-        y_rotation = 180.0,
+        y_rotation = 205.0,
         z_rotation = 180.0,
     },
     ["weapon_dagger"] = {
@@ -153,7 +153,7 @@ BackItems = {
         model="p_ld_heist_bag_s", 
         back_bone = 24818,
         x = -0.05,
-        y = -0.1,
+        y = -0.07,
         z = 0.0,
         x_rotation = 90.0,
         y_rotation = -90.0,
@@ -201,3 +201,104 @@ BackItems = {
         z_rotation = 0.0,
     },
 }
+
+-- Vitto, l'original est en bas
+--[[
+BackItems = {
+    ["markedbills"] = { -- Item Name
+        model="prop_money_bag_01", -- Model you want to display
+        back_bone = 24818, -- PED bone the entity is attached to.
+        -- Location of the model on the players back. Note this is and offset relative to the players bone coords.
+        x = -0.6,
+        y = -0.15,
+        z = -0.28,
+        -- Rotaion of the object.
+        x_rotation = 0.0,
+        y_rotation = 70.0,
+        z_rotation = 0.0,
+    },
+
+    ["meth"] = {
+        model="hei_prop_pill_bag_01", 
+        back_bone = 24818,
+        x = -0.3,
+        y = -0.15,
+        z = 0.05,
+        x_rotation = 0.0,
+        y_rotation = 90.0,
+        z_rotation = 0.0,
+    },
+    
+    ["weapon_smg"] = {
+        model="w_sb_smg", 
+        back_bone = 24818,
+        x = 0.0,
+        y = -0.17,
+        z = -0.12,
+        x_rotation = 0.0,
+        y_rotation = -180.0,
+        z_rotation = 180.0,
+    },
+    ["weapon_assaultrifle"] = {
+        model="w_ar_assaultrifle",
+        back_bone = 24818,
+        x = 0.0,
+        y = -0.17,
+        z = -0.05,
+        x_rotation = 0.0,
+        y_rotation = -180.0,
+        z_rotation = 180.0,
+    },
+    ["weapon_carbinerifle"] = {
+        model="w_ar_carbinerifle", 
+        back_bone = 24818,
+        x = 0.0,
+        y = -0.17,
+        z = 0.08,
+        x_rotation = 0.0,
+        y_rotation = -180.0,
+        z_rotation = 180.0,
+    },
+    ["weapon_rpg"] = {
+        model="w_lr_rpg", 
+        back_bone = 24818,
+        x = 0.2,
+        y = -0.17,
+        z = 0.0,
+        x_rotation = 0.0,
+        y_rotation = 180.0,
+        z_rotation = 180.0,
+    },
+    ["coke_brick"] = {
+        model="bkr_prop_coke_cutblock_01", 
+        back_bone = 24818,
+        x = -0.20,
+        y = -0.17,
+        z = 0.0,
+        x_rotation = 0.0,
+        y_rotation = 90.0,
+        z_rotation = 90.0,
+    },
+    ["weed_bud"] = {
+        model="bkr_prop_weed_drying_01a", 
+        back_bone = 24818,
+        x = -0.20,
+        y = -0.17,
+        z = 0.0,
+        x_rotation = 0.0,
+        y_rotation = 90.0,
+        z_rotation = 0.0,
+    },
+    -- Vitto
+    ["skateboard"] = {
+        model="p_defilied_ragdoll_01_s", 
+        back_bone = 24818,
+        x = 0.05,
+        y = -0.25,
+        z = 0.05,
+        x_rotation = 268.0,
+        y_rotation = 180.0,
+        z_rotation = 0.0,
+    },
+}
+--]]
